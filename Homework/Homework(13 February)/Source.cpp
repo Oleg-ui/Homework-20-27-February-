338cc737b83ec8d55eb635a0e1e13e7d739f3b77
Oleg <olejka647@gmail.com>
2020-03-05 21:18:39 +0300

    Final 28F

--- Homework/Homework(13 February)/Source.cpp
+++ Homework/Homework(13 February)/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <queue> 
 using namespace std;
 
 void show(vector<vector<int>> listG) //Выводит на консоль список смежности
@@ -81,6 +83,72 @@ vector<vector<int>> f(vector<vector<bool>> matrixG) //преобразует матрицу смежно
 	return tmp;
 }
 
+void dfs(vector<vector<int>> listG, int start)         //Поиск в глубину. Проходит через каждую вершину
+{
+	cout << "Поиск в глубину: ";
+	stack<int> Stack;
+	vector<bool> used = vector<bool>(listG.size());
+
+	Stack.push(start);
+
+	while (!Stack.empty())
+	{
+		int v = Stack.top();
+
+		if (used[v] == false)
+		{
+			used[v] = true;
+			cout << v << " ";
+		}
+
+		Stack.pop();
+
+		for (int i = 0; i < listG[v].size(); i++)
+		{
+			if (used[listG[v][i]]==false)
+			{
+				Stack.push(listG[v][i]);
+			}
+		}
+	}
+	cout << endl;
+		
+}
+
+void bfs(vector<vector<int>> listG, int start)      //Поиск в ширину. Проходит по вершинам, но рассматривает именно каждый подуровень графа
+{
+	cout << "Поиск в ширину: ";
+	vector<bool> used = vector<bool>(listG.size());
+	int* dist = new int[listG.size()];
+	int* p = new int[listG.size()];
+
+	p[start] = start;
+	dist[start] = 0;
+
+	queue<int> q;
+	q.push(start);
+
+	while (!q.empty()) {
+		int u = q.front();
+		cout << q.front() << " ";
+		q.pop();
+		for (int i = 0; i < (int)listG[u].size(); i++) {
+			int v = listG[u][i];
+			if (!used[v] && listG[u][i] != start) {
+				used[v] = true;
+				p[v] = u;
+				dist[v] = dist[u] + 1;
+				q.push(v);
+			}
+		}
+	}
+	cout << endl;
+	for (int i = 0; i < (int)listG.size(); i++)
+	{
+		cout << "Расстояние до " << i << "-ой верины: " << dist[i] << endl;
+	}
+}
+
 
 
 int main()
@@ -90,7 +158,7 @@ int main()
 	vector<vector<bool>> matrix;
 	tmp[0] = { 1, 2, 3, 4 };
 	tmp[1] = { 0, 2, 4 };
-	tmp[2] = { 1 };
+	tmp[2] = { 0, 1 };
 	tmp[3] = { 0, 4 };
 	tmp[4] = { 0, 1, 3 };
 
@@ -100,4 +168,6 @@ int main()
 	show(matrix);
 	tmp = f(matrix);
 	show(tmp);
+	dfs(tmp, 0);
+	bfs(tmp, 0);
 }
